fcdb57faaa5acfe4c69d1947dc5a1b8d00d8c778
alex <michaelalexanderrustan@gmail.com>
2022-09-06 22:09:48 +0700

    Updated

--- src/main/kotlin/com/example/demo/TryApplication.kt
+++ src/main/kotlin/com/example/demo/TryApplication.kt
@@ -1,9 +1,10 @@
 package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude=[DataSourceAutoConfiguration::class])
 class TryApplication
 
 fun main(args: Array<String>) {
